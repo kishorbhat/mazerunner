349fb0995a5b32f6b015441a873ce4b9588a72d1
Kishor Bhat <kishorbhat@gmail.com>
2016-08-07 21:38:08 +0530

    Phase 3: Trolls

--- README.rst
+++ README.rst
@@ -7,11 +7,16 @@ This game is my submission for the `Escape the Trolls <https://www.reddit.com/r/
 ====
 TODO
 ====
-* [ ] Pathfinding trolls
+* [X] Pathfinding trolls
+
+  * Ultra basic; need to figure out how to optimize A*/BFS so that running it on each move doesn't take forever.
+
 * [ ] Maze generation
 * [ ] Make the trolls crushable
 * [ ] UTF-8 boxes for walls (instead of '#'s)
 * [ ] Display "Game Over" if player blocks off exit
+* [ ] Colorize
+* [ ] Try making troll movement time-based
 
 =======
 LICENSE
--- mazerunner/mazerunner.py
+++ mazerunner/mazerunner.py
@@ -1,19 +1,33 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 import random
 import curses
 import time
 import sys
-
+import pdb
 
 grid = []
 player_pos = {}
+trolls = []
 exit_pos = {}
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(1)
 
+
+def getEmptySpace(width, height):
+    """Returns a random empty spot in the maze."""
+    while True:
+        x = random.randint(0, width - 1)
+        y = random.randint(0, height - 1)
+        if grid[y][x] == ' ':
+            return x, y
+
+
 def init():
-    """Read maze from file and place the player in a random spot."""
+    """Read maze from file and place player and troll in random spots."""
     with open("./mazes/rakkar16.txt", "r") as f:
         for line in f:
             row = list(line.strip())
@@ -27,15 +41,13 @@ def init():
             exit_pos['x'] = row.index('X')
             exit_pos['y'] = idx
 
-    while(True):
-        x = random.randint(0, width)
-        y = random.randint(0, height)
+    player_pos['x'], player_pos['y'] = getEmptySpace(width, height)
+    grid[player_pos['y']][player_pos['x']] = '^'
 
-        if grid[y][x] == ' ':
-            grid[y][x] = '^'
-            player_pos['x'] = x
-            player_pos['y'] = y
-            break
+    for t in range(10):
+        x, y = getEmptySpace(width, height)
+        grid[y][x] = 'T'
+        trolls.append({'x': x, 'y': y})
 
 
 def isBorderBlock(x, y):
@@ -58,6 +70,61 @@ def render():
     screen.refresh()
 
 
+def moveTrolls():
+    """Move trolls towards player."""
+    for troll in trolls:
+
+        grid[troll['y']][troll['x']] = ' '
+
+        trollDir = ''
+        possibilities = []
+        moved = False
+
+        if troll['x'] == player_pos['x'] and troll['y'] == player_pos['y']:
+            print 'YOU WERE EATEN'
+            sys.exit(0)
+
+        if (troll['x'] - player_pos['x']) > 0:
+            trollDir += 'l'
+        elif (troll['x'] - player_pos['x']) < 0:
+            trollDir += 'r'
+
+        if (troll['y'] - player_pos['y']) > 0:
+            trollDir += 'u'
+        elif (troll['y'] - player_pos['y']) < 0:
+            trollDir += 'd'
+
+        for ch in trollDir:
+            if ch == 'u':
+                possibilities.append((troll['x'], troll['y'] - 1))
+            elif ch == 'd':
+                possibilities.append((troll['x'], troll['y'] + 1))
+            elif ch == 'l':
+                possibilities.append((troll['x'] - 1, troll['y']))
+            elif ch == 'r':
+                possibilities.append((troll['x'] + 1, troll['y']))
+
+        for p in possibilities:
+            if grid[p[1]][p[0]] in (' ', 'v', '>', '<', '^'):
+                troll['x'] = p[0]
+                troll['y'] = p[1]
+                grid[p[1]][p[0]] = 'T'
+                moved = True
+                break
+
+        if not moved:
+            while True:
+                x = troll['x'] + [-1, 0, 1][random.randint(0, 2)]
+                y = troll['y'] + [-1, 0, 1][random.randint(0, 2)]
+
+                if grid[y][x] == ' ':
+                    grid[troll['y']][troll['x']] = ' '
+                    troll['x'] = x
+                    troll['y'] = y
+                    grid[y][x] = 'T'
+                    break
+
+
 def pushBlock(x, y):
     """If given x, y is empty, place a block there."""
     if grid[y][x] == ' ':
@@ -65,18 +132,23 @@ def pushBlock(x, y):
         return True
     return False
 
+
 def updatePlayerPosition(direction):
     """Updates the grid depending on direction entered by user."""
     oldX = player_pos['x']
     oldY = player_pos['y']
 
+    if grid[oldY][oldX] == 'T':
+        print 'YOU WERE EATEN'
+        sys.exit(0)
+
     if direction == 'u':
         if grid[oldY][oldX] != '^':
             grid[oldY][oldX] = '^'
             return
-        if grid[oldY-1][oldX] == '#':
-            if not isBorderBlock(oldX, oldY-1):
-                if not pushBlock(oldX, oldY-2):
+        if grid[oldY - 1][oldX] == '#':
+            if not isBorderBlock(oldX, oldY - 1):
+                if not pushBlock(oldX, oldY - 2):
                     return
             else:
                 return
@@ -86,9 +158,9 @@ def updatePlayerPosition(direction):
         if grid[oldY][oldX] != 'v':
             grid[oldY][oldX] = 'v'
             return
-        if grid[oldY+1][oldX] == '#':
-            if not isBorderBlock(oldX, oldY+1):
-                if not pushBlock(oldX, oldY+2):
+        if grid[oldY + 1][oldX] == '#':
+            if not isBorderBlock(oldX, oldY + 1):
+                if not pushBlock(oldX, oldY + 2):
                     return
             else:
                 return
@@ -98,9 +170,9 @@ def updatePlayerPosition(direction):
         if grid[oldY][oldX] != '<':
             grid[oldY][oldX] = '<'
             return
-        if grid[oldY][oldX-1] == '#':
-            if not isBorderBlock(oldX-1, oldY):
-                if not pushBlock(oldX-2, oldY):
+        if grid[oldY][oldX - 1] == '#':
+            if not isBorderBlock(oldX - 1, oldY):
+                if not pushBlock(oldX - 2, oldY):
                     return
             else:
                 return
@@ -110,9 +182,9 @@ def updatePlayerPosition(direction):
         if grid[oldY][oldX] != '>':
             grid[oldY][oldX] = '>'
             return
-        if grid[oldY][oldX+1] == '#':
-            if not isBorderBlock(oldX+1, oldY):
-                if not pushBlock(oldX+2, oldY):
+        if grid[oldY][oldX + 1] == '#':
+            if not isBorderBlock(oldX + 1, oldY):
+                if not pushBlock(oldX + 2, oldY):
                     return
             else:
                 return
@@ -143,6 +215,7 @@ def gameLoop():
             screen.keypad(0)
             curses.echo()
             sys.exit(0)
+        moveTrolls()
         render()
 
 if __name__ == "__main__":
